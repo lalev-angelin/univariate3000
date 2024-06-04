1aefae644d181ffb013d2e838f9b8595fc8bb05f
Angelin Lalev <lalev@uni-svishtov.bg>
2024-06-04 13:41:37 +0300

    Finished refactoring on LightGBMForecast.py

--- LightGBMForecast.py
+++ LightGBMForecast.py
@@ -1,6 +1,7 @@
 from GBDTForecast import GBDTForecast
 from lightgbm import LGBMRegressor
 from sklearn.multioutput import MultiOutputRegressor
+import numpy as np
 
 class LightGBMForecast(GBDTForecast):
 
@@ -11,23 +12,6 @@ class LightGBMForecast(GBDTForecast):
             lookback : int = 1,
             number_of_subperiods : int = None,
             starting_subperiod : int = 1, 
-            boosting_type: str = 'gbdt',
-            num_leaves: int = 31, 
-            max_depth: int = -1, 
-            min_data_in_leaf: int = None,
-            learning_rate: float = 0.1,
-            n_estimators: int = 100, 
-            subsample_for_bin: int = 200000, 
-            class_weight : dict = None, 
-            min_split_gain: float = 0, 
-            min_child_weight: float = 1e-3, 
-            min_child_samples: int =  20, 
-            subsample: float = 1.0,
-            subsample_freq: int = 0, 
-            reg_alpha: float = 0.0,
-            reg_lambda: float = 0.0,
-            random_state: int = None, 
-            n_jobs: int = None,
             **kwargs):
         
         super().__init__(timeseries, 
@@ -37,23 +21,6 @@ class LightGBMForecast(GBDTForecast):
                      number_of_subperiods = number_of_subperiods,
                      starting_subperiod = starting_subperiod)
 
-        self._boosting_type = boosting_type
-        self._num_leaves = num_leaves
-        self._max_depth = max_depth
-        self._min_data_in_leaf=min_data_in_leaf
-        self._learning_rate = learning_rate
-        self._n_estimators = n_estimators 
-        self._subsample_for_bin = subsample_for_bin
-        self._class_weight = class_weight
-        self._min_split_gain = min_split_gain 
-        self._min_child_weight = min_child_weight 
-        self._min_child_samples = min_child_samples 
-        self._subsample = subsample
-        self._subsample_freq = subsample_freq 
-        self._reg_alpha = reg_alpha 
-        self._reg_lambda = reg_lambda 
-        self._random_state = random_state
-        self._n_jobs = n_jobs 
         self._kwargs = kwargs
 
         
@@ -73,29 +40,23 @@ class LightGBMForecast(GBDTForecast):
          
 
         regressor = LGBMRegressor(
-                boosting_type=self._boosting_type,
-                num_leaves=self._num_leaves,
-                max_depth=self._max_depth,
-                min_data_in_leaf=self._min_data_in_leaf,
-                learning_rate=self._learning_rate,
-                n_estimators=self._n_estimators,
-                subsample_for_bin=self._subsample_for_bin,
-                class_weight=self._class_weight,
-                min_split_gain=self._min_split_gain,
-                min_child_weight=self._min_child_weight,
-                min_child_samples=self._min_child_samples, 
-                subsample=self._subsample,
-                subsample_freq=self._subsample_freq,
-                reg_alpha=self._reg_alpha,
-                reg_lambda=self._reg_lambda,
-                random_state=self._random_state,
-                n_jobs=self._n_jobs,
                 **self._kwargs)
         
         multi_target_regressor = MultiOutputRegressor(regressor)
 
         multi_target_regressor.fit(trainX, trainY)
 
-        forecast = multi_target_regressor.predict(input_segments) 
+        preliminary = multi_target_regressor.predict(input_segments) 
 
-        
+        self._forecast = [np.NaN] * self._lookback
+        self._forecast = self._forecast + [x[0] for x in preliminary[:-1]]
+        self._forecast = self._forecast + list(preliminary[-1])
+       
+        assert len(self._forecast)==len(self._timeseries), "Internal error when compiling the forecast"
+
+    def asses_forecast_mape(self) -> float:
+            self.compute_mape(self._timeseries, self._forecast, from_position = len(self._timeseries) - self._forecast_horizon)
+
+    def asses_forecast_rmse(self) -> float:
+            self.compute_rmse(self._timeseries, self._forecast, from_position = len(self._timeseries) - self._forecast_horizon)
+
--- UnitTestLightGBMForecast.py
+++ UnitTestLightGBMForecast.py
@@ -5,6 +5,9 @@ forecast = LightGBMForecast(list(range(1, 50)),
             lookback=6, 
             num_leaves=2,
             max_depth=6, 
+            min_data_in_leaf=2,
+            min_data_in_bin=2
             )
 
 forecast.generate_forecast()
+print(forecast.as_list())
